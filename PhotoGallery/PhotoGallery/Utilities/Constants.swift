7516e2c827c3610a20226ea221aad486c429378f
Clamwash <clam.wash@gmail.com>
2023-07-13 14:15:10 +0200

    Update constants usage

--- PhotoGallery/PhotoGallery/Utilities/Constants.swift
+++ PhotoGallery/PhotoGallery/Utilities/Constants.swift
@@ -14,3 +14,23 @@ struct Constants {
         static let largeBold = UIFont.boldSystemFont(ofSize: 20)
     }
 }
+
+extension Constants {
+    struct Strings {
+        struct LaunchScreen {
+            static let appIconTitle = "App-icon-move-01"
+            
+            static let title = "iOS assignment: Photo gallery"
+        }
+        
+        struct MainScreen {
+            static let accessibilityIdentifier = "MainScreenTitleLabel"
+
+            static let errorText = "Failed to fetch photos. Please check your internet connection."
+        }
+        
+        struct DetailScreen {
+            static let accessibilityIdentifier = "DetailScreenTitleLabel"
+        }
+    }
+}
--- PhotoGallery/PhotoGallery/Views/LaunchScreenViewController.swift
+++ PhotoGallery/PhotoGallery/Views/LaunchScreenViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 import PureLayout
 
 class LaunchScreenViewController: UIViewController {
+    private let logoImageSize = CGSize(width: 350, height: 350)
+    
     var logoImageView: UIImageView!
     var titleLabel: UILabel!
     
@@ -11,19 +13,19 @@ class LaunchScreenViewController: UIViewController {
         view.backgroundColor = .black
 
         logoImageView = UIImageView.newAutoLayout()
-        logoImageView.image = UIImage(named: "App-icon-move-01")
+        logoImageView.image = UIImage(named: Constants.Strings.LaunchScreen.appIconTitle)
         logoImageView.contentMode = .scaleAspectFit
         view.addSubview(logoImageView)
 
         titleLabel = UILabel.newAutoLayout()
-        titleLabel.text = "iOS assignment: Photo gallery"
+        titleLabel.text = Constants.Strings.LaunchScreen.title
         titleLabel.textAlignment = .center
         titleLabel.textColor = .white
         view.addSubview(titleLabel)
         
         logoImageView.autoAlignAxis(toSuperviewAxis: .vertical)
         logoImageView.autoAlignAxis(.horizontal, toSameAxisOf: view, withOffset: -30)
-        logoImageView.autoSetDimensions(to: CGSize(width: 350, height: 350))
+        logoImageView.autoSetDimensions(to: logoImageSize)
 
         titleLabel.autoAlignAxis(toSuperviewAxis: .vertical)
         titleLabel.autoPinEdge(.top, to: .bottom, of: logoImageView, withOffset: Constants.CGFloats.small)
